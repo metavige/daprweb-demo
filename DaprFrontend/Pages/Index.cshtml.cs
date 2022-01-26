b8c8451c499e01244afa83930c606f7ab1e32059
metavige <metavige@gmail.com>
2022-01-26 15:08:53 +0800

    proj init

---
+++ DaprFrontend/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DaprFrontend.Pages;
+
+public class IndexModel : PageModel
+{
+  private readonly DaprClient _daprClient;
+
+  public IndexModel(DaprClient daprClient)
+  {
+    _daprClient = daprClient;
+  }
+
+  public async Task OnGet()
+  {
+    var forecasts = await _daprClient.InvokeMethodAsync<IEnumerable<WeatherForecast>>(
+      HttpMethod.Get,
+      "DaprBackend",
+      "weatherforecast");
+
+    ViewData["WeatherForecastData"] = forecasts;
+  }
+}
